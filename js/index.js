4832518a5cacb387c01aef5e56fef4f247c10804
Nikita-quartZ <Flashfly16@gmail.com>
2021-02-14 19:45:32 +0400

    mini

--- js/index.js
+++ js/index.js
@@ -41,4 +41,10 @@ window.addEventListener('DOMContentLoaded', function() {
             e.preventDefault();
         });
     });
+    // jQuery(document).ready(function() {
+    //     jQuery('.post').addClass("hidden").viewportChecker({
+    //     classToAdd: 'visible animated fadeIn',
+    //     offset: 100
+    //     });
+    // });
 })
